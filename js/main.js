4ac0dc6f7ea43c38fa7d66d428b3f0d5338fc76c
Hoi Nguyen <nmhoi@mmsofts.com>
2015-10-05 21:48:00 +0700

    + Add script read json data

--- js/main.js
+++ js/main.js
@@ -1,85 +1,121 @@
+function processPopUpContent(jsonData) {
+  var node = document.getElementById('canhoskycentertap1');
+  node.innerHTML = "<p>"+jsonData.project[0].data[0].info[0].text+"</p>"
+}
+
+
+function loadJSON(callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'data/data.json', true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function() {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);
+}
+
+function init() {
+    loadJSON(function(response) {
+        // Parse JSON string into object
+        var actual_JSON = JSON.parse(response);
+        console.log(actual_JSON);
+        processPopUpContent(actual_JSON);
+    });
+}
+
+
 jQuery(document).ready(function($) {
 
-	'use strict';
+    'use strict';
 
 
-	/************** Toggle *********************/
+    /************** Toggle *********************/
     // Cache selectors
     var lastId,
         topMenu = $(".menu-first, .menu-responsive"),
-        topMenuHeight = topMenu.outerHeight()+15,
+        topMenuHeight = topMenu.outerHeight() + 15,
         // All list items
         menuItems = topMenu.find("a"),
         // Anchors corresponding to menu items
-        scrollItems = menuItems.map(function(){
-          var item = $($(this).attr("href"));
-          if (item.length) { return item; }
+        scrollItems = menuItems.map(function() {
+            var item = $($(this).attr("href"));
+            if (item.length) {
+                return item;
+            }
         });
 
     // Bind click handler to menu items
     // so we can get a fancy scroll animation
-    menuItems.click(function(e){
-      var href = $(this).attr("href"),
-          offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
-      $('html, body').stop().animate({ 
-          scrollTop: offsetTop
-      }, 300);
-      e.preventDefault();
+    menuItems.click(function(e) {
+        var href = $(this).attr("href"),
+            offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight + 1;
+        $('html, body').stop().animate({
+            scrollTop: offsetTop
+        }, 300);
+        e.preventDefault();
     });
 
     // Bind to scroll
-    $(window).scroll(function(){
-       // Get container scroll position
-       var fromTop = $(this).scrollTop()+topMenuHeight;
-       
-       // Get id of current scroll item
-       var cur = scrollItems.map(function(){
-         if ($(this).offset().top < fromTop)
-           return this;
-       });
-       // Get the id of the current element
-       cur = cur[cur.length-1];
-       var id = cur && cur.length ? cur[0].id : "";
-       
-       if (lastId !== id) {
-           lastId = id;
-           // Set/remove active class
-           menuItems
-             .parent().removeClass("active")
-             .end().filter("[href=#"+id+"]").parent().addClass("active");
-       }                   
+    $(window).scroll(function() {
+        // Get container scroll position
+        var fromTop = $(this).scrollTop() + topMenuHeight;
+
+        // Get id of current scroll item
+        var cur = scrollItems.map(function() {
+            if ($(this).offset().top < fromTop)
+                return this;
+        });
+        // Get the id of the current element
+        cur = cur[cur.length - 1];
+        var id = cur && cur.length ? cur[0].id : "";
+
+        if (lastId !== id) {
+            lastId = id;
+            // Set/remove active class
+            menuItems
+                .parent().removeClass("active")
+                .end().filter("[href=#" + id + "]").parent().addClass("active");
+        }
     });
 
 
 
-    $(window).scroll(function(){
-         $('.main-header').toggleClass('scrolled', $(this).scrollTop() > 1);
-     });
+    $(window).scroll(function() {
+        $('.main-header').toggleClass('scrolled', $(this).scrollTop() > 1);
+    });
 
 
 
-    $('a[href="#top"]').click(function(){
-        $('html, body').animate({scrollTop: 0}, 'slow');
+    $('a[href="#top"]').click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 'slow');
         return false;
     });
 
 
     $('.flexslider').flexslider({
-      slideshow: true,
-      slideshowSpeed: 3000,  
-      animation: "fade",
-      directionNav: false,
+        slideshow: true,
+        slideshowSpeed: 3000,
+        animation: "fade",
+        directionNav: false,
     });
 
 
-    $('.toggle-menu').click(function(){
+    $('.toggle-menu').click(function() {
         $('.menu-responsive').slideToggle();
         return false;
     });
 
 
     /************** LightBox *********************/
-      $(function(){
+    $(function() {
         $('[data-rel="lightbox"]').lightbox();
-      });
+    });
+
+    /************* READ DATA *********************/
+    init();
 });
